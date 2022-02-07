8db51205a7b3df7b9f64877a621a2665fa5786e5
jpmason64 <jpmason@quinnipiac.edu>
2022-02-07 18:18:08 -0500

    initial commit

---
+++ app/src/main/java/edu/quinnipiac/ser210/mymessenger/ReceiveMessageActivity.java
@@ -0,0 +1,20 @@
+package edu.quinnipiac.ser210.mymessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra("message");
+        TextView messageView = (TextView) findViewById(R.id.result_text);
+        messageView.setText(messageText);
+    }
+}
